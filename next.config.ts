ca3a9c055a6a839c96681781c5f143cd9330dcba
Vishwajeet Kondi <vkondi@gmail.com>
2025-07-06 20:14:32 +0530

    fix: lib path error

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   rewrites: async () => {
@@ -11,6 +12,13 @@ const nextConfig: NextConfig = {
             : "/api/",
       },
     ];
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
   },
 };
 
